dd865d780733f1045a6940c81f191badb2323e02
igaimerca <igaimerca@gmail.com>
2022-11-15 11:16:26 +0200

    feat: add and remove job in progress

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,76 +1,94 @@
+import { useState } from "react";
+
 function Page({ jsonData }) {
     if (!jsonData) return <h1>No data fetched! </h1>
+    const [data, setData] = useState(jsonData)
     const handleChange = (e) => {
         let value = e.target.innerHTML;
         let name = e.target.name;
 
         console.log(value, name);
     }
-    return (
-        <div id="resume-container">
-            <header id="header">
-                <div>
-                    <div className="title">
-                        <div className="names">
-                        <h1 contentEditable suppressContentEditableWarning>{jsonData.firstName}</h1>
-                        <h1 contentEditable suppressContentEditableWarning>{jsonData.lastName}</h1>
-                        </div>
-                        <h3 contentEditable suppressContentEditableWarning>{jsonData.position}</h3>
-                    </div>
 
+    const removeJob = () => {
+
+    }
+    const addJob = () => {
+       
+    }
+
+return (
+    <div id="resume-container">
+        <header id="header">
+            <div>
+                <div className="title">
+                    <div className="names">
+                        <h1 contentEditable suppressContentEditableWarning>{data.firstName}</h1>
+                        <h1 contentEditable suppressContentEditableWarning>{data.lastName}</h1>
+                    </div>
+                    <h3 contentEditable suppressContentEditableWarning>{data.position}</h3>
+                </div>
+
+                <div>
+                    <div className="contact-info">
+                        {data.social_links?.map((social, i) => (
+                            <p key={i}>
+                                <b>{social.title}: </b><a contentEditable suppressContentEditableWarning href={social.link}>{social.link}</a></p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <main id="main-container">
+            <div>
+                <div>
                     <div>
-                        <div className="contact-info">
-                            {jsonData.social_links?.map((social, i) => (
-                                <p contentEditable suppressContentEditableWarning key={i}>
-                                    <b>{social.title}: </b><a href={social.link}>{social.link}</a></p>
+                        <div className="title">
+                            <h2>Objective</h2>
+                        </div>
+                        <div>
+                            <p className="enlarge" contentEditable suppressContentEditableWarning onInput={handleChange}>
+                                {data.objective}
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="title">
+                            <h2>Experience</h2>
+                        </div>
+
+                        <div>
+                            {data.experience.map((exp, index) => (
+                                <div className="job" key={index}>
+                                    <ul className="add-remove">
+                                        <li className="remove" onClick={removeJob}>
+                                            -
+                                        </li>
+                                        {index == data.experience.length - 1 ? <li className="add" onClick={addJob}>+</li> : ""}
+
+                                    </ul>
+                                    <h3 className='company' contentEditable suppressContentEditableWarning onInput={handleChange}>{exp.company}</h3>
+                                    <div className="job-desc">
+                                        <p className='position' contentEditable suppressContentEditableWarning onInput={handleChange}>{exp.position}</p>
+                                        <p className='timeline' contentEditable suppressContentEditableWarning onInput={handleChange}>{exp.startDate} &mdash; {exp.endDate}</p>
+                                    </div>
+                                    <p contentEditable suppressContentEditableWarning onInput={handleChange}>{exp.description} </p>
+                                </div>
                             ))}
                         </div>
                     </div>
+
                 </div>
-            </header>
-
-            <main id="main-container">
-                <div>
-                    <div>
-                        <div>
-                            <div className="title">
-                                <h2>Objective</h2>
-                            </div>
-                            <div>
-                                <p className="enlarge" contentEditable suppressHydrationWarning onInput={handleChange}>
-                                    {jsonData.objective}
-                                </p>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="title">
-                                <h2>Experience</h2>
-                            </div>
-
-                            <div>
-                                {jsonData.experience.map((exp, index) => (
-                                    <div className="job last" key={index}>
-                                        <h3 className='company' contentEditable suppressHydrationWarning onInput={handleChange}>{exp.company}</h3>
-                                        <div className="job-desc">
-                                            <p className='position' contentEditable suppressHydrationWarning onInput={handleChange}>{exp.position}</p>
-                                            <p className='timeline' contentEditable suppressHydrationWarning onInput={handleChange}>{exp.startDate} &mdash; {exp.endDate}</p>
-                                        </div>
-                                        <p contentEditable suppressHydrationWarning onInput={handleChange}>{exp.description} </p>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </main>
-
-            <div id="footer">
-                <p>{jsonData.firstName + " " + jsonData.lastName} &mdash; <a href="mailto:name@yourdomain.com">{jsonData.email}</a> &mdash; (250) 790 888 345</p>
             </div>
+        </main>
 
+        <div id="footer">
+            <p>{data.firstName + " " + data.lastName} &mdash; <a href="mailto:name@yourdomain.com">{data.email}</a> &mdash; (250) 790 888 345</p>
         </div>
-    )
+
+    </div>
+)
 }
 
 export default Page
